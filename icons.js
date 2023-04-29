b3dc1e51eb7d4627259b9490037918e186b1f813
Ingrid Lara <113933685+ingrid1112@users.noreply.github.com>
2023-04-29 19:04:43 -0300

    Add files via upload

---
+++ icons.js
@@ -0,0 +1,7 @@
+function redirecionar(){
+    window.open("https://www.linkedin.com/company/normalabs-hub/?originalSubdomain=br");
+    
+}
+function redirecionarInsta(){
+    window.open("https://instagram.com/normalabs.hub?igshid=YmMyMTA2M2Y=")
+}
